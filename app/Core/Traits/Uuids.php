df995b8149b3e80307e4e6ac233696da7b33bf94
Marco Aurelyo <aurelyo.web@gmail.com>
2019-08-04 15:01:15 -0300

    [UPD] Se aplicado esse commit vai Criar trait para models com chave UUID

---
+++ app/Core/Traits/Uuids.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core\Traits;
+
+use Webpatser\Uuid\Uuid;
+
+/**
+ * Trait Uuids
+ * @package App
+ */
+trait Uuids
+{
+
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Uuid::generate()->string;
+        });
+    }
+}
